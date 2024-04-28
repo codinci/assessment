b96656bbd5d0815811737b8d2a06151ac970b4b7
MburuDavid <mburud550@gmail.com>
2024-04-28 09:19:55 +0300

    Added signup and signin pages with firebase

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+	return (
+		<div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+			<h1>Welcome Home</h1>
+		</div>
+	)
+}
+
+export default Home;
